9a55c5d0f5ed3db965f25da5f4db7724869edab7
Jarosław Ogonowski <dza.ogonowski@gmail.com>
2022-10-01 00:04:31 +0200

    add icon and function

--- js/script.js
+++ js/script.js
@@ -2,7 +2,6 @@
     const welcome = () => { console.log("HelloBigBrain!"); }
     welcome();
 }
-
 {
     const form = document.querySelector(".js-form")
     const disableFormSending = () => form.addEventListener("submit", (event) => {
@@ -10,58 +9,57 @@
     })
     disableFormSending();
 }
-
 {
     const form = document.querySelector(".js-form")
+    const receivedAmount = document.querySelector(".js-receivedAmount");
+    const formValidation = () => form.addEventListener("input", () => {
+        receivedAmount.value = "Wprowadź wszystkie dane, a następnie przelicz";
+    })
+    formValidation();
+}
+{
+
     const yourCurrency = document.querySelector(".js-yourCurrency");
     const startSum = document.querySelector(".js-startSum");
     const exchangedCurrency = document.querySelector(".js-exchangedCurrency");
     const receivedAmount = document.querySelector(".js-receivedAmount");
     const button = document.querySelector(".form__button")
 
+    const baseCurrency = () =>
+        button.addEventListener("click", () => {
+            switch (yourCurrency.value) {
+                case "PLN":
+                    return baseCurrencyResult = startSum.value;
+                case "EUR":
+                    return baseCurrencyResult = +startSum.value * 4.8698;
+                case "USD":
+                    return baseCurrencyResult = +startSum.value * 4.9533;
+                case "GBP":
+                    return baseCurrencyResult = +startSum.value * 5.556;
+            }
 
-    button.addEventListener("click", () => {
-        switch (yourCurrency.value) {
-            case "PLN":
-                return baseCurrencyResult = startSum.value;
-            case "EUR":
-                return baseCurrencyResult = +startSum.value * 4.8698;
-            case "USD":
-                return baseCurrencyResult = +startSum.value * 4.9533;
-            case "GBP":
-                return baseCurrencyResult = +startSum.value * 5.556;
-        }
-
-    })
+        })
 
     exchangedProces = () => {
         const form = document.querySelector(".js-form")
         button.addEventListener("click", () => {
             if (exchangedCurrency.value === "exchangedPLN") {
-                receivedAmount.value = (+baseCurrencyResult).toFixed(2);
+                receivedAmount.value = (+baseCurrencyResult).toFixed(2) + " PLN";
 
             } else if (exchangedCurrency.value === "exchangedEUR") {
-                receivedAmount.value = (+baseCurrencyResult * 0.2053).toFixed(2);
+                receivedAmount.value = (+baseCurrencyResult * 0.2053).toFixed(2) + " EUR";
 
             }
             else if (exchangedCurrency.value === "exchangedUSD") {
-                receivedAmount.value = (+baseCurrencyResult * 0.2019).toFixed(2);
+                receivedAmount.value = (+baseCurrencyResult * 0.2019).toFixed(2) + " USD";
             }
             else if (exchangedCurrency.value === "exchangedGBP") {
-                receivedAmount.value = (+baseCurrencyResult * 0.18).toFixed(2);
+                receivedAmount.value = (+baseCurrencyResult * 0.18).toFixed(2) + " GBP";
             }
 
         })
     };
-
-
-
-
-    form.addEventListener("input", () => {
-        receivedAmount.value = "Wprowadź wszystkie dane, a następnie przelicz";
-    })
-
-    
+    baseCurrency();
     exchangedProces();
 
 }
